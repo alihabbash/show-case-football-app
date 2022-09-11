f667e4e7ea96285843b564930366610a5442463f
Ali Habbash <ali.aleppo.95@gmail.com>
2022-09-11 16:44:09 +0300

    Add logging to the requests

--- lib/core/api/dio_wrapper.dart
+++ lib/core/api/dio_wrapper.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:football/core/api/api_const.dart';
 import 'package:football/core/api/api_keys.dart';
 import 'package:football/core/exceptions/app_exceptions.dart';
+import 'package:logger/logger.dart';
 
 class DioWrapper {
   static final _dioWrapper = DioWrapper._();
@@ -17,10 +20,17 @@ class DioWrapper {
         ApiKeys.authHeader: ApiConst.apiKey,
       });
     dioClient.interceptors.add(_requestsInterceptor);
+    dioClient.interceptors.add(_loggingInterceptor);
     // dioClient.interceptors.add(_cacheInterceptor);
   }
 
   factory DioWrapper() => _dioWrapper;
+
+  LogInterceptor get _loggingInterceptor => LogInterceptor(
+        responseBody: true,
+        requestBody: true,
+        logPrint: Logger().i,
+      );
 
   InterceptorsWrapper get _requestsInterceptor => InterceptorsWrapper(
         onError: (e, handler) {
@@ -38,14 +48,14 @@ class DioWrapper {
         },
       );
 
-  // DioCacheInterceptor get _cacheInterceptor {
-  //   final options = CacheOptions(
-  //       store: MemCacheStore(),
-  //       priority: CachePriority.normal,
-  //       maxStale: const Duration(days: 1),
-  //       keyBuilder: CacheOptions.defaultCacheKeyBuilder);
-  //   return DioCacheInterceptor(
-  //     options: options,
-  //   );
-  // }
+// DioCacheInterceptor get _cacheInterceptor {
+//   final options = CacheOptions(
+//       store: MemCacheStore(),
+//       priority: CachePriority.normal,
+//       maxStale: const Duration(days: 1),
+//       keyBuilder: CacheOptions.defaultCacheKeyBuilder);
+//   return DioCacheInterceptor(
+//     options: options,
+//   );
+// }
 }
