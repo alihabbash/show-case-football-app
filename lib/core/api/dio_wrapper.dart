6fad338b67e1c7d3fe2b353f8185c4156d0b59b4
Ali Habbash <ali.aleppo.95@gmail.com>
2022-09-11 11:01:13 +0300

    Add exception handling to dio client

--- lib/core/api/dio_wrapper.dart
+++ lib/core/api/dio_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:football/core/api/api_const.dart';
 import 'package:football/core/api/api_keys.dart';
+import 'package:football/core/exceptions/app_exceptions.dart';
 
 class DioWrapper {
   static final _dioWrapper = DioWrapper._();
@@ -14,6 +15,21 @@ class DioWrapper {
       ..options.headers.addAll({
         ApiKeys.authHeader: ApiConst.apiKey,
       });
+    dioClient.interceptors.add(InterceptorsWrapper(
+      onError: (e, handler) {
+        if (e.response == null) {
+          throw NoInternetException();
+        }
+        if (e.response!.statusCode == 403) {
+          throw UnauthorisedException();
+        }
+        if (e.response!.statusCode == 499 || e.response!.statusCode == 500) {
+          throw BadRequestException();
+        }
+        throw FetchDataException();
+        // handler.next(e);
+      },
+    ));
   }
 
   factory DioWrapper() => _dioWrapper;
